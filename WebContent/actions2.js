14705ecc1760a125e4f2826d805d1d7947d99d11
SaulVelazquezAmador <sv_amador_14@outlook.com>
2020-05-25 20:17:18 -0500

    Menu principal casi completo, solo falta agregarle iconos

--- WebContent/actions2.js
+++ WebContent/actions2.js
@@ -1,5 +1,123 @@
 $(document).ready(function(){
-    $("#op_1").mouseover(function(){
+
+    $("#op_1").click(function(){
         $(this).css('background-color', '#23303e');
+        $("#op_2").css('background-color', '#222222');
+        $("#op_3").css('background-color', '#222222');
+        $("#op_4").css('background-color', '#222222');
+        $("#op_5").css('background-color', '#222222');
+        $("#op_6").css('background-color', '#222222');
+        $("#op_7").css('background-color', '#222222');
+        $("#op_8").css('background-color', '#222222');
+        $("#op_9").css('background-color', '#222222');
+        $("#op_10").css('background-color', '#222222');
+    });
+    $("#op_2").click(function(){
+        $(this).css('background-color', '#23303e');
+        $("#op_1").css('background-color', '#222222');
+        $("#op_3").css('background-color', '#222222');
+        $("#op_4").css('background-color', '#222222');
+        $("#op_5").css('background-color', '#222222');
+        $("#op_6").css('background-color', '#222222');
+        $("#op_7").css('background-color', '#222222');
+        $("#op_8").css('background-color', '#222222');
+        $("#op_9").css('background-color', '#222222');
+        $("#op_10").css('background-color', '#222222');
+    });
+    $("#op_3").click(function(){
+        $(this).css('background-color', '#23303e');
+        $("#op_1").css('background-color', '#222222');
+        $("#op_2").css('background-color', '#222222');
+        $("#op_4").css('background-color', '#222222');
+        $("#op_5").css('background-color', '#222222');
+        $("#op_6").css('background-color', '#222222');
+        $("#op_7").css('background-color', '#222222');
+        $("#op_8").css('background-color', '#222222');
+        $("#op_9").css('background-color', '#222222');
+        $("#op_10").css('background-color', '#222222');
+    });
+    $("#op_4").click(function(){
+        $(this).css('background-color', '#23303e');
+        $("#op_1").css('background-color', '#222222');
+        $("#op_2").css('background-color', '#222222');
+        $("#op_3").css('background-color', '#222222');
+        $("#op_5").css('background-color', '#222222');
+        $("#op_6").css('background-color', '#222222');
+        $("#op_7").css('background-color', '#222222');
+        $("#op_8").css('background-color', '#222222');
+        $("#op_9").css('background-color', '#222222');
+        $("#op_10").css('background-color', '#222222');
+    });
+    $("#op_5").click(function(){
+        $(this).css('background-color', '#23303e');
+        $("#op_1").css('background-color', '#222222');
+        $("#op_2").css('background-color', '#222222');
+        $("#op_3").css('background-color', '#222222');
+        $("#op_4").css('background-color', '#222222');
+        $("#op_6").css('background-color', '#222222');
+        $("#op_7").css('background-color', '#222222');
+        $("#op_8").css('background-color', '#222222');
+        $("#op_9").css('background-color', '#222222');
+        $("#op_10").css('background-color', '#222222');
+    });
+    $("#op_6").click(function(){
+        $(this).css('background-color', '#23303e');
+        $("#op_1").css('background-color', '#222222');
+        $("#op_2").css('background-color', '#222222');
+        $("#op_3").css('background-color', '#222222');
+        $("#op_4").css('background-color', '#222222');
+        $("#op_5").css('background-color', '#222222');
+        $("#op_7").css('background-color', '#222222');
+        $("#op_8").css('background-color', '#222222');
+        $("#op_9").css('background-color', '#222222');
+        $("#op_10").css('background-color', '#222222');
+    });
+    $("#op_7").click(function(){
+        $(this).css('background-color', '#23303e');
+        $("#op_1").css('background-color', '#222222');
+        $("#op_2").css('background-color', '#222222');
+        $("#op_3").css('background-color', '#222222');
+        $("#op_4").css('background-color', '#222222');
+        $("#op_5").css('background-color', '#222222');
+        $("#op_6").css('background-color', '#222222');
+        $("#op_8").css('background-color', '#222222');
+        $("#op_9").css('background-color', '#222222');
+        $("#op_10").css('background-color', '#222222');
+    });
+    $("#op_8").click(function(){
+        $(this).css('background-color', '#23303e');
+        $("#op_1").css('background-color', '#222222');
+        $("#op_2").css('background-color', '#222222');
+        $("#op_3").css('background-color', '#222222');
+        $("#op_4").css('background-color', '#222222');
+        $("#op_5").css('background-color', '#222222');
+        $("#op_6").css('background-color', '#222222');
+        $("#op_7").css('background-color', '#222222');
+        $("#op_9").css('background-color', '#222222');
+        $("#op_10").css('background-color', '#222222');
+    });
+    $("#op_9").click(function(){
+        $(this).css('background-color', '#23303e');
+        $("#op_1").css('background-color', '#222222');
+        $("#op_2").css('background-color', '#222222');
+        $("#op_3").css('background-color', '#222222');
+        $("#op_4").css('background-color', '#222222');
+        $("#op_5").css('background-color', '#222222');
+        $("#op_6").css('background-color', '#222222');
+        $("#op_7").css('background-color', '#222222');
+        $("#op_8").css('background-color', '#222222');
+        $("#op_10").css('background-color', '#222222');
+    });
+    $("#op_10").click(function(){
+        $(this).css('background-color', '#23303e');
+        $("#op_1").css('background-color', '#222222');
+        $("#op_2").css('background-color', '#222222');
+        $("#op_3").css('background-color', '#222222');
+        $("#op_4").css('background-color', '#222222');
+        $("#op_5").css('background-color', '#222222');
+        $("#op_6").css('background-color', '#222222');
+        $("#op_7").css('background-color', '#222222');
+        $("#op_8").css('background-color', '#222222');
+        $("#op_9").css('background-color', '#222222');
     });
 });
